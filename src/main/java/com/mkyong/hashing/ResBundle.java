cf1ee2dc211edf628ad7f4f81c23c911321d6920
Amey P <boetraining123@gmail.com>
2019-06-28 05:26:35 +0000

    test

--- src/main/java/com/mkyong/hashing/ResBundle.java
+++ src/main/java/com/mkyong/hashing/ResBundle.java
@@ -3,6 +3,11 @@ package com.mkyong.hashing;
 import java.util.ResourceBundle;
 
 public class ResBundle {
+
+	private ResBundle() {
+    		throw new IllegalStateException("Resource Bundle class");
+  	}
+
 	public static String getBundle(String key) {
 		ResourceBundle rs = ResourceBundle.getBundle("global");
 		return rs.getString(key);
