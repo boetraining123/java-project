8276316ac02a89cb5c07edc80e2511fbb3e4202d
Amey P <boetraining123@gmail.com>
2019-06-28 05:22:32 +0000

    test

--- src/main/java/com/mkyong/hashing/ResBundle.java
+++ src/main/java/com/mkyong/hashing/ResBundle.java
@@ -2,7 +2,7 @@ package com.mkyong.hashing;
 
 import java.util.ResourceBundle;
 
-public class Res_Bundle {
+public class ResBundle {
 	public static String getBundle(String key) {
 		ResourceBundle rs = ResourceBundle.getBundle("global");
 		return rs.getString(key);
